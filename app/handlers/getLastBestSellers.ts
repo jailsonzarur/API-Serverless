403dbee780db0fc81f2b16b1477d40d4889d08f7
jailsonzarur <jailsoncardoz123@gmail.com>
2025-03-24 19:45:19 -0300

    refactor: change typescript file name.

---
+++ app/handlers/getLastBestSellers.ts
@@ -0,0 +1,15 @@
+import { BestSellerRepository } from "../database/repositories/bestseller-repository"
+import { httpResponse } from "../utils/HttpResponses";
+
+const repository = new BestSellerRepository()
+
+export async function handler(event: any) {
+
+    try {
+        const data = await repository.get_last_top3_bestsellers();
+        return httpResponse.success(data);
+    } catch (error) {
+        return httpResponse.error("Internal Server Error");
+    }
+
+}
