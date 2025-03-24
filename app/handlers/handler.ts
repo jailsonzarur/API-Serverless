288dcb1c29fd07b204c410f591a527c5ed39719d
jailsonzarur <jailsoncardoz123@gmail.com>
2025-03-24 19:54:41 -0300

    refactor: chenage the health check response.

--- app/handlers/handler.ts
+++ app/handlers/handler.ts
@@ -2,7 +2,10 @@ import { httpResponse } from "../utils/HttpResponses";
 
 export async function handler(event: any) {
     try {
-        return httpResponse.success(null)
+        return httpResponse.success({
+            message: 'API online',
+            timestamp: new Date().toISOString(),
+        })
     } catch(error) {
         return httpResponse.error("Internal Server Error")
     }
